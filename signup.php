389793e35f984016c0e9422952f26ad95ff28a3e
Keziah <heyneskeziah@gmail.com>
2019-11-01 14:08:00 +0200

    look for the signup page

--- signup.php
+++ signup.php
@@ -3,22 +3,22 @@
   <header>
     <link rel="stylesheet" type="text/css" href="styles/index.css">
     <meta charset="UTF-8">
-    <title>CAMAGRU</title>
+    <title>SIGNUP</title>
   </header>
   <body>
     <?php include('header.php') ?>
     <?php include('footer.php') ?>
     <div id="login">
-      <div class="title">LOGIN</div>
-        <form method="post" style="position: relative;" action="login.php">
+      <div class="title">SIGNUP</div>
+        <form method="post" style="position: relative;" action="signup.php">
           <label>Email: </label>
           <input id="mail" name="email" placeholder="email" type="mail">
+          <label>Username: </label>
+          <input id="name" name="username" placeholder="username" type="text">
           <label>Password: </label>
           <input id="password" name="password" placeholder="password" type="password">
-          <input name="submit" type="submit" value=" SIGN IN ">
-          <a href="signup.php">Create account</a>
-          <a href="forgot.php">Forget password ?</a>
+          <input name="submit" type="submit" value=" SEND ">
         </form>
     </div>
   </body>
-</HTML
+</HTML>
