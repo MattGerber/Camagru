cba8ec2ef82f04e4f0f9476480abe151ffc69e2d
Keziah <heyneskeziah@gmail.com>
2019-11-04 20:43:35 +0200

    created a final version of a header hopefully

--- header.php
+++ header.php
@@ -1,6 +1,9 @@
-
-    <div class="button" onclick="location.href='index.php'">
-      <span>
-        login
-      </span>
-    </div>
+      <div id = "head">
+        <div class="banner">
+          <img src = "imgs/icon.png" alt = "icon" height = 60 width = 60>
+          <i style="font-family:Brush Script MT, cursive; font-size:300%;">CAMAGRU</i>
+        </div>
+        <div class="button" onclick="location.href='index.php'">
+            logout ?
+        </div>
+      </div>
