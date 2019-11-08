8416361a6c464aaa35a1dd5039bfd70c556757de
Keziah Heynes <heyneskeziah@gmail.com>
2019-11-08 15:53:37 +0200

    integrated matthews front end stuffies.

--- header.php
+++
@@ -1,9 +0,0 @@
-      <div id = "head">
-        <div class="banner">
-          <img src = "imgs/icon.png" alt = "icon" height = 60 width = 60>
-          <i style="font-family:Brush Script MT, cursive; font-size:300%;">CAMAGRU</i>
-        </div>
-        <div class="button" onclick="location.href='index.php'">
-            login
-        </div>
-      </div>
--- index.php
+++ index.php
@@ -1,29 +1,40 @@
 <?php
-    session_start();
-
-    include "functions/login.php"
+    require "header1.php"
 ?>
-<!DOCTYPE html>
-<HTML>
-  <header>
-    <link rel="stylesheet" type="text/css" href="styles/index.css">
-    <meta charset="UTF-8">
-    <title>CAMAGRU</title>
-  </header>
-  <body>
-    <?php include('header.php') ?>
-    <?php include('footer.php') ?>
-    <div id="login">
-      <div class="title">LOGIN</div>
-        <form method="post" style="position: relative;">
-          <label>Email: </label>
-          <input id="mail" name="email" placeholder="email" type="mail">
-          <label>Password: </label>
-          <input id="password" name="passwd" placeholder="password" type="password">
-          <input name="submit" type="submit" value=" SIGN IN ">
-          <a href="signup.php">Create account</a>
-          <a href="forgot.php">Forgot password ?</a>
-        </form>
-    </div>
-  </body>
-</HTML
+<main class="hero is-fullheight has-background-dark">
+	<!-- <div class="container has-text-centered "> -->
+		<!-- <div class="box is-7 is-large has-background-grey-lighter "style="margin-top: 250px;"> -->
+		<!-- <div class="container is-large"> -->
+			<!-- <form action="includes/login.inc.php" method="post"> -->
+				<!-- <div class="field"> -->
+					<!-- <p class="control has-icons-left has-icons-right"> -->
+						<!-- <input class="input" type="email" name="mailuid" placeholder="Email">  -->
+						<!-- <span class="icon is-small is-left"> -->
+							<!-- <i class="fas fa-envelope"></i> -->
+						<!-- </span> -->
+						<!-- <span class="icon is-small is-right"> -->
+							<!-- <i class="fas fa-check"></i> -->
+						<!-- </span> -->
+					<!-- </p> -->
+				<!-- </div> -->
+				<!-- <div class="field"> -->
+					<!-- <p class="control has-icons-left"> -->
+						<!-- <input class="input" type="password" name="pwd" placeholder="Password"> -->
+						<!-- <span class="icon is-small is-left"> -->
+							<!-- <i class="fas fa-lock"></i> -->
+						<!-- </span> -->
+					<!-- </p> -->
+				<!-- </div> -->
+				<!-- <div class="field"> -->
+					<!-- <p class="control"> -->
+						<!-- <button class="button is-danger" name="login-submit" type="submit">Login</button> -->
+					<!-- </p> -->
+				<!-- </div> -->
+			<!-- </form> -->
+			<!-- <form action="gallery.php" method="post"> -->
+				<!-- <button class="button is-danger" name="login-submit" type="submit">Login</button> -->
+			<!-- </form> -->
+		<!-- </div> -->
+		<!-- </div> -->
+	<!-- </div> -->
+</main>
