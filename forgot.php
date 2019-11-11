314f8cf6a0232e950c6bc25590ad92bd3902f623
Keziah Heynes <heyneskeziah@gmail.com>
2019-11-11 12:38:21 +0200

    struggling to make the box on the forgot pass page look right.

--- forgot.php
+++ forgot.php
@@ -1,17 +1,34 @@
+<?php include('header3.php') ?>
 <!DOCTYPE html>
 <HTML>
   <header>
     <title>FORGOT PASSWORD</title>
   </header>
-  <body>
-    <?php include('header2.php') ?>
-    <div id="login">
-      <div class="title">FORGOT PASSWORD</div>
-        <form method="post" style="position: relative;" action="forgot.php">
-          <label>Email: </label>
-          <input id="mail" name="email" placeholder="email" type="mail">
-          <input name="submit" type="submit" value=" SEND ">
-        </form>
+  <main class="hero is-fullheight has-background-dark">
+    <div class="container has-text-centered">
+      <div class="box is-7 is-large has-background-grey-lighter" style="margin-top: 300px;">
+        <div class="container is-large">
+        <form method="POST" action="forgot.php">
+          <div class="field">
+          <label class="label">FORGOT PASSWORD</label>
+            <div class="control has-icons-left has-icons-right">
+              <input name="email" type="email" placeholder="Email Adress">
+              <span class="icon is-small is-left">
+                <i class="fas fa-envelope"></i>
+              </span>
+            </div>
+          </div>
+            <div class="control">
+              <button type="submit" name="submit" class="button">SEND</button>
+            </div>
+        </div>
+      <div>
+      </form>
+        <!-- <form method="post" style="position: relative;" action="forgot.php"> -->
+          <!-- <label>Email: </label> -->
+          <!-- <input id="mail" name="email" placeholder="email" type="mail"> -->
+          <!-- <input name="submit" type="submit" value=" SEND "> -->
+        <!-- </form> -->
     </div>
-  </body>
+  </main>
 </HTML>
