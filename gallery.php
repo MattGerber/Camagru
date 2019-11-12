63073b1f2a32b2301f9371d4c5a59ff46282553d
Keziah Heynes <heyneskeziah@gmail.com>
2019-11-12 16:32:38 +0200

    ignore the yucky ducky sidebar for now

--- gallery.php
+++ gallery.php
@@ -5,7 +5,26 @@
 ?>
 
 <main class="hero is-fullheight has-background-dark">
-    <div style="color: white">
-        Welcome!!
+    <a class="button is-danger" href="postImage.php" style="margin-right: 1100px; margin-top: 5px; margin-left: 5px">
+        <strong>Add a Photo</strong>
+    </a>
+    <div class="container has-text-centered">
+      <div class="box is-7 is-large has-background-dark" style="margin-top: 5px;">
+        <div class="container is-large">
+        <div class="card">
+            <div class="card-image">
+                <figure class="image is-100x100">
+                  <img src="https://unsplash.it/300/200/?random&pic=1" alt="" style="height:400px; width:600px;">
+                </figure>
+            </div>
+            <footer class="card-footer">
+                <a class="card-footer-item">
+                  Like
+                </a>
+            </footer>
+        </div>
     </div>
+    </div>
+        </div>
+      </div>
 </main>
--- postImage.php
+++ postImage.php
@@ -23,4 +23,91 @@
 </div>
 </div>
     </form>
+    <html>
+<body>
+
+<!-- <nav class="nav has-shadow"> -->
+  <div class="container">
+    <!-- <div class="nav-left"> -->
+      <a class="nav-item">
+        Previous Images
+      </a>
+    </div>
+    <label for="menu-toggle" class="nav-toggle">
+      <span></span>
+      <span></span>
+      <span></span>
+    </label>
+    <input type="checkbox" id="menu-toggle" class="is-hidden"/>
+    <div class="nav-right nav-menu">
+      <a class="nav-item is-tab is-hidden-tablet">
+        <span class="icon"><i class="fa fa-home"></i></span> Home
+      </a>
+      <a class="nav-item is-tab is-hidden-tablet">
+        <span class="icon"><i class="fa fa-table"></i></span> Links
+      </a>
+      <a class="nav-item is-tab is-hidden-tablet">
+        <span class="icon"><i class="fa fa-info"></i></span> About
+      </a>
+      
+      <a class="nav-item is-tab is-active">
+        <span class="icon"><i class="fa fa-user"></i></span>
+      </a>
+      <a class="nav-item is-tab">
+        <span class="icon"><i class="fa fa-sign-out"></i></span>
+      </a>
+    </div>
+  </div>
+</nav>
+
+<section class="main-content columns is-fullheight">
+  
+  <aside class="column is-2 is-narrow-mobile is-fullheight section is-hidden-mobile">
+    <p class="menu-label is-hidden-touch">Navigation</p>
+    <ul class="menu-list">
+      <li>
+        <a href="#" class="">
+          <span class="icon"><i class="fa fa-home"></i></span> Home
+        </a>
+      </li>
+      <li>
+        <a href="#" class="is-active">
+          <span class="icon"><i class="fa fa-table"></i></span> Links
+        </a>
+
+        <ul>
+          <li>
+            <a href="#">
+              <span class="icon is-small"><i class="fa fa-link"></i></span> Link1
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <span class="icon is-small"><i class="fa fa-link"></i></span> Link2
+            </a>
+          </li>
+        </ul>
+      </li>
+      <li>
+        <a href="#" class="">
+          <span class="icon"><i class="fa fa-info"></i></span> About
+        </a>
+      </li>
+    </ul>
+  </aside>
+    </div>
+  </div>
+  
+</section>
+
+<footer class="footer is-hidden">
+  <div class="container">
+    <div class="content has-text-centered">
+      <p>Hello</p>
+    </div>
+  </div>
+</footer>
+    
+</body>
+</html>
 </main>
