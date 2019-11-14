3258d124d3c2a4964e820a274160153a24b36af2
Keziah Heynes <heyneskeziah@gmail.com>
2019-11-14 10:04:26 +0200

    side bar made everything else yucky ducky now

--- postImage.php
+++ postImage.php
@@ -3,12 +3,25 @@
 ?>
 
 <main class="hero is-fullheight has-background-dark">
-    <div class="container has-test-center">
+  
+<div class="columns is-fullheight" style="margin-top: 10px; margin-left:10px;">
+  <div class="column is-2 is-sidebar-menu is-hidden-mobile">
+    <aside class="menu has-background-grey-lighter">
+  <p class="menu-label">
+    General
+  </p>
+  <ul class="menu-list">
+    <li><a>Dashboard</a></li>
+    <li><a>Customers</a></li>
+  </ul>
+</aside>
+  </div>
+  <div class="column is-main-content">
+  <div class="container has-text-centered">
     <div class="box is-7 is-large has-background-grey-lighter" style="margin-top: 50px;">
     <div class="container is-large">
     <table class="table is-bordered">
-    <!-- needs to go to an upload function -->
-    <form action="functions/uploadImage.func.php" method="post" enctype="multipart/form-data">
+<form action="functions/uploadImage.func.php" method="post" enctype="multipart/form-data">
     <div class="column has-text-centered">
         <label class="label">Add Photo</label>
     </div>
@@ -19,95 +32,10 @@
         </tbody>
     </table>
     <button type="submit" name="post" class="button is-danger">Post</button>
-</div>
-</div>
-</div>
     </form>
-    <html>
-<body>
-
-<!-- <nav class="nav has-shadow"> -->
-  <div class="container">
-    <!-- <div class="nav-left"> -->
-      <a class="nav-item">
-        Previous Images
-      </a>
-    </div>
-    <label for="menu-toggle" class="nav-toggle">
-      <span></span>
-      <span></span>
-      <span></span>
-    </label>
-    <input type="checkbox" id="menu-toggle" class="is-hidden"/>
-    <div class="nav-right nav-menu">
-      <a class="nav-item is-tab is-hidden-tablet">
-        <span class="icon"><i class="fa fa-home"></i></span> Home
-      </a>
-      <a class="nav-item is-tab is-hidden-tablet">
-        <span class="icon"><i class="fa fa-table"></i></span> Links
-      </a>
-      <a class="nav-item is-tab is-hidden-tablet">
-        <span class="icon"><i class="fa fa-info"></i></span> About
-      </a>
-      
-      <a class="nav-item is-tab is-active">
-        <span class="icon"><i class="fa fa-user"></i></span>
-      </a>
-      <a class="nav-item is-tab">
-        <span class="icon"><i class="fa fa-sign-out"></i></span>
-      </a>
-    </div>
+</div>
+</div>
+</div>
   </div>
-</nav>
-
-<section class="main-content columns is-fullheight">
-  
-  <aside class="column is-2 is-narrow-mobile is-fullheight section is-hidden-mobile">
-    <p class="menu-label is-hidden-touch">Navigation</p>
-    <ul class="menu-list">
-      <li>
-        <a href="#" class="">
-          <span class="icon"><i class="fa fa-home"></i></span> Home
-        </a>
-      </li>
-      <li>
-        <a href="#" class="is-active">
-          <span class="icon"><i class="fa fa-table"></i></span> Links
-        </a>
-
-        <ul>
-          <li>
-            <a href="#">
-              <span class="icon is-small"><i class="fa fa-link"></i></span> Link1
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <span class="icon is-small"><i class="fa fa-link"></i></span> Link2
-            </a>
-          </li>
-        </ul>
-      </li>
-      <li>
-        <a href="#" class="">
-          <span class="icon"><i class="fa fa-info"></i></span> About
-        </a>
-      </li>
-    </ul>
-  </aside>
-    </div>
-  </div>
-  
-</section>
-
-<footer class="footer is-hidden">
-  <div class="container">
-    <div class="content has-text-centered">
-      <p>Hello</p>
-    </div>
-  </div>
-</footer>
-    
-</body>
-</html>
+</div>
 </main>
