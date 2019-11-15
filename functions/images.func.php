78c9a050fec8bcbc3245df447b82577321af9323
kheynes <heyneskeziah@gmail.com>
2019-11-15 14:50:02 +0200

    user profile

--- functions/images.func.php
+++ functions/images.func.php
@@ -14,12 +14,11 @@ function display_all_photos($id) {
 			$get_all_photos->execute();
 			$data = $get_all_photos->fetchAll(PDO::FETCH_ASSOC);
 			if ($data) {
-				echo "adsfadsf";
 				foreach ($data as $image) {
 				   echo "<div class = 'box column is-7 is-offset-one-quarter'>
 					<br />
 					   <h1 class='subtitle is-3 has-text-centered'>
-						   <a href=''><img style='height:480; width:480;' src=data:image/png;base64,".base64_encode($image['source'])."></a>
+						   <a href=''><img style='height:480; width:640;' src=data:image/png;base64,".base64_encode($image['source'])."></a>
 					   </h1>
 					<br />
 					</div>";
